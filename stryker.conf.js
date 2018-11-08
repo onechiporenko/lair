9a5f5d4b124b2c3c82b0abdd3ce94719647a0728
onechiporenko <oo.nechiporenko@gmail.com>
2018-11-08 19:44:02 +0200

    Fix stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,15 +1,17 @@
 module.exports = function(config) {
   config.set({
-    files: [],
     testRunner: "mocha",
     mutator: "typescript",
     transpilers: ["typescript"],
     tsconfigFile: "tsconfig.json",
-    reporter: ["clear-text", "progress", "html"],
+    reporters: ["clear-text", "progress", "html"],
     testFramework: "mocha",
     coverageAnalysis: "off",
     // logLevel: "trace",
     mutate: ["lib/**/*.ts", "!lib/decorators.ts"],
+    mochaOptions: {
+      files: ["dist/tests/**/*.js"]
+    },
     thresholds: {break: 50, high: 80, low: 60}
   });
 };
